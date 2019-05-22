1e242985b988a04fd4420ce4130481e414c796f9
woojin <rapid.pwn@gmail.com>
2019-05-22 05:40:19 +0000

    hmm

--- src/wc.c
+++ src/wc.c
@@ -2,22 +2,151 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdint.h>
+#ifdef __GNUC__
+#define FORCE_INLINE __attribute__((always_inline)) inline
+#else
+#define FORCE_INLINE inline
+#endif
 
-#include <sys/queue.h>
-
-static LIST_HEAD(listhead, entry) head;
-
-struct listhead *headp = NULL;
-int num_entries = 0;
-
-struct entry {
-	char name[BUFSIZ];
-	int frequency;
-	LIST_ENTRY(entry) entries;
-};
-
-int main(int argc, char** argv)
+static FORCE_INLINE uint32_t rotl32 ( uint32_t x, int8_t r )
 {
+  return (x << r) | (x >> (32 - r));
+}
+
+static FORCE_INLINE uint64_t rotl64 ( uint64_t x, int8_t r )
+{
+  return (x << r) | (x >> (64 - r));
+}
+
+#define	ROTL32(x,y)	rotl32(x,y)
+#define ROTL64(x,y)	rotl64(x,y)
+
+#define BIG_CONSTANT(x) (x##LLU)
+
+//-----------------------------------------------------------------------------
+// Block read - if your platform needs to do endian-swapping or can only
+// handle aligned reads, do the conversion here
+
+#define getblock(p, i) (p[i])
+
+//-----------------------------------------------------------------------------
+// Finalization mix - force all bits of a hash block to avalanche
+
+static FORCE_INLINE uint32_t fmix32 ( uint32_t h )
+{
+  h ^= h >> 16;
+  h *= 0x85ebca6b;
+  h ^= h >> 13;
+  h *= 0xc2b2ae35;
+  h ^= h >> 16;
+
+  return h;
+}
+
+//----------
+
+static FORCE_INLINE uint64_t fmix64 ( uint64_t k )
+{
+  k ^= k >> 33;
+  k *= BIG_CONSTANT(0xff51afd7ed558ccd);
+  k ^= k >> 33;
+  k *= BIG_CONSTANT(0xc4ceb9fe1a85ec53);
+  k ^= k >> 33;
+
+  return k;
+}
+
+//-----------------------------------------------------------------------------
+
+void MurmurHash3_x86_32 ( const void * key, int len,
+                          uint32_t seed, void * out )
+{
+  const uint8_t * data = (const uint8_t*)key;
+  const int nblocks = len / 4;
+  int i;
+
+  uint32_t h1 = seed;
+
+  uint32_t c1 = 0xcc9e2d51;
+  uint32_t c2 = 0x1b873593;
+
+  //----------
+  // body
+
+  const uint32_t * blocks = (const uint32_t *)(data + nblocks*4);
+
+  for(i = -nblocks; i; i++)
+  {
+    uint32_t k1 = getblock(blocks,i);
+
+    k1 *= c1;
+    k1 = ROTL32(k1,15);
+    k1 *= c2;
+    
+    h1 ^= k1;
+    h1 = ROTL32(h1,13); 
+    h1 = h1*5+0xe6546b64;
+  }
+
+  //----------
+  // tail
+
+  const uint8_t * tail = (const uint8_t*)(data + nblocks*4);
+
+  uint32_t k1 = 0;
+
+  switch(len & 3)
+  {
+  case 3: k1 ^= tail[2] << 16;
+  case 2: k1 ^= tail[1] << 8;
+  case 1: k1 ^= tail[0];
+          k1 *= c1; k1 = ROTL32(k1,15); k1 *= c2; h1 ^= k1;
+  };
+
+  //----------
+  // finalization
+
+  h1 ^= len;
+
+  h1 = fmix32(h1);
+
+  *(uint32_t*)out = h1;
+}
+Map * globalMap = NULL; 
+typedef struct _HashElem{
+	char * key;
+	uint32_t hash;
+}HashElem
+typedef struct _Map{
+	HashElem hash;
+	uint32_t value;
+}Map;
+
+void allocate_Map() {
+	globalMap = (Map *)mmap(0, sizeof(Map)* 0x10000,PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
+	memset((void *)globalMap, 0x0, sizeof(Map)*0x10000);
+}
+void do_Map(char * key, int32_t index){
+	globalMap[index].hash.key = key;
+	MurmurHash3_x86_32(key, strlen(key), 0x13371337, &globalMap[index].hash.hash)
+	globalMap[index].value = 1;
+}
+void do_Reduce(int32_t index){
+	Map * tMap = &globalMap[index];
+	if(tMap->hash.hash == 0)
+		return;
+	for(int32_t i = 0; i < index -1; i++){
+		if( tMap->hash.hash == globalMap[i].hash.hash){
+			globalMap[i].value++;
+			memset((void *)&globalMap[index], 0x0, sizeof(Map));
+			return;
+		}
+	}
+	globalMap[index].value++;
+	return;
+}
+int main(int argc, char ** argv){
 	if (argc != 2) {
 		fprintf(stderr, "%s: not enough input\n", argv[0]);
 		exit(1);
@@ -25,150 +154,4 @@ int main(int argc, char** argv)
 
 	FILE* fp = fopen(argv[1], "r");
 	char buf[4096];
-
-	LIST_INIT(&head);
-
-	while (fgets(buf, 4096, fp)) {
-		// Preprocess: change all non-alnums into white-space,
-		//             alnums to lower-case.
-		int line_length = strlen(buf);
-
-		for (int it = 0; it < line_length; ++it) {
-			if (!isalnum(buf[it])) {
-				buf[it] = ' ';
-			} else {
-				buf[it] = tolower(buf[it]);
-			}
-		}
-
-    // Tokenization
-		const char* WHITE_SPACE =" \t\n";
-		char* tok = strtok(buf, WHITE_SPACE);
-
-		if (tok == NULL || strcmp(tok, "") == 0) {
-			continue;
-		}
-
-		do {
-      if (num_entries == 0) {
-        struct entry* e = malloc(sizeof(struct entry));
-
-        strncpy(e->name, tok, strlen(tok));
-        e->frequency = 1;
-
-        LIST_INSERT_HEAD(&head, e, entries);
-        num_entries++;
-        continue;
-      } else if (num_entries == 1) {
-        int cmp = strcmp(tok, head.lh_first->name);
-
-        if (cmp == 0) {
-          head.lh_first->frequency++;
-        } else if (cmp > 0) {
-          struct entry* e = malloc(sizeof(struct entry));
-
-          strncpy(e->name, tok, strlen(tok));
-          e->frequency = 1;
-
-
-          LIST_INSERT_AFTER(head.lh_first, e, entries);
-          num_entries++;
-        } else if (cmp < 0) {
-          struct entry* e = malloc(sizeof(struct entry));
-
-          strncpy(e->name, tok, strlen(tok));
-          e->frequency = 1;
-
-          LIST_INSERT_HEAD(&head, e, entries);
-          num_entries++;
-        }
-
-        continue;
-      }
-
-      // Reduce: actual word-counting
-      struct entry* np = head.lh_first;
-      struct entry* final_np = NULL;
-
-      int last_cmp = strcmp(tok, np->name);
-
-      if (last_cmp < 0) {
-        struct entry* e = malloc(sizeof(struct entry));
-
-        strncpy(e->name, tok, strlen(tok));
-        e->frequency = 1;
-
-        LIST_INSERT_HEAD(&head, e, entries);
-        num_entries++;
-
-        continue;
-
-      } else if (last_cmp == 0) {
-        np->frequency++;
-
-        continue;
-      }
-
-      for (np = np->entries.le_next; np != NULL; np = np->entries.le_next) {
-        int cmp = strcmp(tok, np->name);
-
-        if (cmp == 0) {
-          np->frequency++;
-          break;
-        } else if (last_cmp * cmp < 0) { // sign-crossing occurred
-          struct entry* e = malloc(sizeof(struct entry));
-
-          strncpy(e->name, tok, strlen(tok));
-          e->frequency = 1;
-
-          LIST_INSERT_BEFORE(np, e, entries);
-          num_entries++;
-
-          break;
-        }
-
-        if (np->entries.le_next == NULL) {
-          final_np = np;
-        } else {
-          last_cmp = cmp;
-        }
-      }
-
-      if (!np && final_np) {
-        struct entry* e = malloc(sizeof(struct entry));
-
-        strncpy(e->name, tok, strlen(tok));
-        e->frequency = 1;
-
-        LIST_INSERT_AFTER(final_np, e, entries);
-        num_entries++;
-      }
-    } while (tok = strtok(NULL, WHITE_SPACE));
-  }
-
-  // Print the counting result very very slow way.
-  int max_frequency = 0;
-
-  for (struct entry* np = head.lh_first; np != NULL; np = np->entries.le_next) {
-    if (max_frequency < np->frequency) {
-      max_frequency = np->frequency;
-    }
-  }
-
-  for (int it = max_frequency; it > 0; --it) {
-    for (struct entry* np = head.lh_first; np != NULL; np = np->entries.le_next) {
-      if (np->frequency == it) {
-        printf("%s %d\n", np->name, np->frequency);
-      }
-    }
-  }
-
-  // Release
-  while (head.lh_first != NULL) {
-    LIST_REMOVE(head.lh_first, entries);
-  }
-
-  fclose(fp);
-
-  return 0;
 }
